51ee5e851a95bbc3a06f92a40dab64650e0dd2c1
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-07-14 15:55:59 +0800

    first draft

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+// all request will have to go thru this middleware
+app.use(express.static('public'))
+// app.use(function (req, res, next) {
+//   console.log('first middleware')
+//   next() // important to continue to next function
+// })
+//
+// app.use(function (req, res, next) {
+//   console.log('second middleware')
+// })
+
+// listening for request
+app.get('/', function (req, res) {
+  res.sendFile('index.html')
+})
+
+app.get('/about/:name', function (req, res) {
+  res.send(req.params)
+})
+
+app.get('/faq', function (req, res) {
+  res.send('faq page')
+})
+
+app.listen(3000, function () {
+  console.log('express is running on port 3000')
+})
